b078b121b2ad38db0ef0dc7fc253d64369e34e11
evan <emaczura@neuronic.dev>
2025-11-30 16:09:31 -0600

    misc: tell clippy the world is unsafe in ebpf. include ebpf code in global cargo test runs

--- xlb-ebpf/src/lib.rs
+++ xlb-ebpf/src/lib.rs
@@ -1,4 +1,6 @@
 #![cfg_attr(not(test), no_std)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 
-mod net;
+pub mod net;
 pub mod utils;
--- xlb-ebpf/src/main.rs
+++ xlb-ebpf/src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
 
 mod net;
 mod utils;
